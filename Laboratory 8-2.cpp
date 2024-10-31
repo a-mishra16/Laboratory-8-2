18f723e5db5178ce0046ffbd160c18ae93d1d383
Aanika Mishra <a-mishra@onu.edu>
2024-10-31 09:03:13 -0400

    Add project files.

---
+++ Laboratory 8-2.cpp
@@ -0,0 +1,15 @@
+/*
+* Program: Laboratory 8-2.cpp
+* Author: Aanika Mishra
+* Date: 10-31-2024
+* Description: This program counts and returns the number of words in a given string 
+*/
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+using namespace std;
+
+int main()
+{
