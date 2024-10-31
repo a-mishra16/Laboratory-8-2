6508de63397c48dd56ac49aaf718513db9907789
Aanika Mishra <a-mishra@onu.edu>
2024-10-31 09:03:53 -0400

    Initialized Visual Studio project.

--- Laboratory 8-2.cpp
+++ Laboratory 8-2.cpp
@@ -2,7 +2,7 @@
 * Program: Laboratory 8-2.cpp
 * Author: Aanika Mishra
 * Date: 10-31-2024
-* Description: This program counts and returns the number of words in a given string 
+* Description: This program counts and returns the number of words in a string given by the user 
 */
 
 #include <iostream>
